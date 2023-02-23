0fede2cc3ffdca2101aa4812fd6035eb2c19896a
Arya TS <aryasreekala1997@gmail.com>
2023-02-23 14:45:20 +0530

    Registration Form complete, it added to the local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import { HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
+import { RegistrationLinkComponent } from './registration-link/registration-link.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationLinkComponent,
+    RegistrationFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    // HttpModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { FormControl,FormGroup,Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.css']
+})
+export class RegistrationFormComponent {
+  alert:boolean=false
+  firstName:string;
+  lastName:string;
+  gender:string;
+  age:number;
+  phoneNumber:number;
+
+  regForm:FormGroup;
+
+constructor(){
+  
+
+}
+ngOnInit(){
+  this.regForm=new FormGroup({
+    'firstname':new FormControl(null,Validators.required),
+    'lastname':new FormControl(null,Validators.required),
+    'gender':new FormControl(null,Validators.required),
+    'age':new FormControl(null,Validators.required),
+    'phonenumber': new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10)
+    ]),
+    
+  });
+  localStorage.setItem('formData', JSON.stringify(this.regForm.value));
+}
+
+
+
+onSubmit(){
+ console.log(this.regForm);
+
+this.alert=true
+// localStorage.setItem('formData', JSON.stringify(this.regForm.value));
+// this.regForm.reset();
+
+let formDataArray = JSON.parse(localStorage.getItem('formData'));
+
+
+  if (!Array.isArray(formDataArray)) {
+    formDataArray = [];
+  }
+
+  
+  formDataArray.push(this.regForm.value);
+
+
+  localStorage.setItem('formData', JSON.stringify(formDataArray));
+
+  
+  this.regForm.reset();
+}
+closeAlert(){
+  this.alert=false
+}
+}
+
+
