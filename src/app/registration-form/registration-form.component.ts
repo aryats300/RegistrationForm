e804e1417414b0f9b8f32b7d920c6be4c753fceb
Arya TS <aryasreekala1997@gmail.com>
2023-02-28 18:09:47 +0530

    Time Added to each data in Registration List

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl,FormGroup,Validators } from '@angular/forms'
+import { RegistrationService } from '../registration.service';
 
 @Component({
   selector: 'app-registration-form',
@@ -8,12 +9,13 @@ import { FormControl,FormGroup,Validators } from '@angular/forms'
 })
 export class RegistrationFormComponent {
   alert:boolean=false
-  firstName:string;
-  lastName:string;
-  gender:string;
-  age:number;
-  phoneNumber:number;
-
+  // firstName:string;
+  // lastName:string;
+  // gender:string;
+  // age:number;
+  // phoneNumber:number;
+  // time:string;
+  // id:number
   regForm:FormGroup;
 
 constructor(){
@@ -22,6 +24,7 @@ constructor(){
 }
 ngOnInit(){
   this.regForm=new FormGroup({
+    'id':new FormControl(0),
     'firstname':new FormControl(null,Validators.required),
     'lastname':new FormControl(null,Validators.required),
     'gender':new FormControl(null,Validators.required),
@@ -29,42 +32,60 @@ ngOnInit(){
     'phonenumber': new FormControl(null, [
       Validators.required,
       Validators.minLength(10),
-      Validators.maxLength(10)
+      Validators.maxLength(10),
+      Validators.pattern(/^[1-9]+[0-9]*$/)
     ]),
     
   });
  
 }
 
+// onSubmit(){
+//  console.log(this.regForm);
 
+// this.alert=true
+// // localStorage.setItem('formData', JSON.stringify(this.regForm.value));
+// // this.regForm.reset();
 
+// let formDataArray = JSON.parse(localStorage.getItem('formData'));
+
+//   if (!Array.isArray(formDataArray)) {
+//     formDataArray = [];
+//   }
+
+//   formDataArray.push(this.regForm.value);
+//   localStorage.setItem('formData', JSON.stringify(formDataArray));
+//   this.regForm.reset();
+// }
+// closeAlert(){
+//   this.alert=false
+// }
+// }
 onSubmit(){
- console.log(this.regForm);
+  console.log(this.regForm);
+  
+  this.alert=true;
 
-this.alert=true
-// localStorage.setItem('formData', JSON.stringify(this.regForm.value));
-// this.regForm.reset();
-
-let formDataArray = JSON.parse(localStorage.getItem('formData'));
-
+  let formDataArray = JSON.parse(localStorage.getItem('formData'));
 
   if (!Array.isArray(formDataArray)) {
     formDataArray = [];
   }
 
+  let lastId = formDataArray.length > 0 ? formDataArray[formDataArray.length - 1].id : 0;
+  let newId = lastId + 1;
+
   
-  formDataArray.push(this.regForm.value);
+  let formValueWithId = { ...this.regForm.value, id: newId };
 
-
+  formDataArray.push(formValueWithId);
   localStorage.setItem('formData', JSON.stringify(formDataArray));
+  // this.regService.setData('users', formDataArray);
 
-  
+
+ 
   this.regForm.reset();
 }
-closeAlert(){
-  this.alert=false
 }
-}
-
 
 
--- src/app/registration-list/registration-list.component.ts
+++ src/app/registration-list/registration-list.component.ts
@@ -14,8 +14,35 @@ export class RegistrationListComponent {
 
   ngOnInit(): void {
     this.dataArray = this.regService.getData('formData');
-    // const userString = localStorage.getItem('users') as string;
-    // const users = JSON.parse(userString);
-    // return users || [];
+   
   }
+  
+
+//   onSaveData(data: { firstname: any; lastname: any; time: any; }) {
+//   console.log('submit');
+//   let StoredData = JSON.parse(localStorage.getItem('formData') as string);
+
+//   for (let i = 0; i < StoredData.length; i++) {
+//     if (StoredData[i].firstname === data.firstname && StoredData[i].lastname === data.lastname) {
+//       StoredData[i].time = data.time;
+//       break;
+//     }
+//   }
+
+//   localStorage.setItem('formData', JSON.stringify(StoredData));
+// }
+onSaveData(data: { id:any,time: any; }) {
+  console.log('submit');
+  let StoredData = JSON.parse(localStorage.getItem('formData') as string);
+
+  for (let i = 0; i < StoredData.length; i++) {
+    if (StoredData[i].id === data.id) {
+      StoredData[i].time = data.time;
+      break;
+    }
+  }
+
+  localStorage.setItem('formData', JSON.stringify(StoredData));
+}
+  
 }
